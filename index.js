26e902579054e632f8e7672749b5da108b49dfa5
alfred kivuva <kivuvakioko97@gmail.com>
2023-12-13 23:21:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const saturdayFun = function(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  };
+  
+
+  const mondayWork = function(task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+  };
+  
+  const wrapAdjective = function(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  };
